e36a71b846a3db13299e6ca98b102cea4932150a
hongzhu <10@zhtec.xyz>
2024-10-22 12:35:02 +0800

    Modified the default configuration

--- main.py
+++ main.py
@@ -19,19 +19,22 @@ DEFAULT_CONFIG = {
     "save_cookies": True,
     "proxies": {},
     "logLevel": "INFO",
+    "tree_view": True,
+    "progressbar_vier": True,
     "qr_extra": {
         "show_in_terminal": None,
         "ensure_unicode": False
     },
+    "image_path":"",
     "pushplus": {
         "enable": False,
         "token": ""
     },
-    "bark": {
+    "bark":{
         "enable": False,
         "token": "https://example.com/xxxxxxxxx"
     },
-    "config_version": "1.3.1",
+    "config_version": "1.3.0",
     "ai": {
         "enabled": True,
         "use_zhidao_ai": True,
